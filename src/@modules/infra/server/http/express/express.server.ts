8f3d69d222bc1c2c53954bce5e51340eebdc8c29
samuel_ricardo <samuelricardoofficial@gmail.com>
2026-02-05 01:55:34 -0300

    [ :sparkles: ] | implements: adapter - express [server:http] (@module::infra)

--- src/@modules/infra/server/http/express/express.server.ts
+++ src/@modules/infra/server/http/express/express.server.ts
@@ -2,26 +2,31 @@ import { inject, injectable } from "inversify";
 import { HttpServer } from "../http.server";
 import { TExpress } from "../../../../../@types/engine/server/http/express.type";
 import { MODULE } from "../../../../app.registry";
+import { DataLogger } from "../../../../../@decorators/log/data.decorator";
+import { HttpMethod } from "../../../../../@types/http/methods.type";
+import {
+  AsyncLogger,
+  LoggableAsync,
+} from "../../../../../@decorators/async/logger.decorator";
 
+@AsyncLogger()
 @injectable()
-export class ExpressServerAdapter implements HttpServer {
+export class ExpressServerAdapter extends LoggableAsync implements HttpServer {
   @inject(MODULE.INFRA.ENGINE.SERVER.HTTP.EXPRESS._)
   private readonly server: TExpress;
 
-  on(
-    method: "post" | "get" | "put" | "delete",
-    url: string,
-    callback: Function,
-  ): void {
+  on(method: HttpMethod, url: string, callback: Function): void {
     this.server[method](url, async function (req, res) {
       const output = await callback(req.params, req.body, req.headers);
       res.json(output);
     });
   }
 
-  listen(port?: number): void {
-    port = port || 3000;
-    console.log(`Listening on http://localhost:${port}`);
-    this.server.listen(port);
+  listen(p: number = 3000): void {
+    this.info({
+      context: "SERVER",
+      message: `Listening on http://localhost:${p}`,
+    });
+    this.server.listen(p);
   }
 }
