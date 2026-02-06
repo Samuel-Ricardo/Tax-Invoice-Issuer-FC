bc379bee1205f9999b859f26903b2beff2ebadfb
samuel_ricardo <samuelricardoofficial@gmail.com>
2026-02-06 13:31:30 -0300

    [ :sparkles: ] | update: util - module > merge -> load (@module::util)

--- src/@utils/module/merge.util.ts
+++
@@ -1,7 +0,0 @@
-import { Container, ContainerModule } from "inversify";
-
-export const merge = (module: ContainerModule[]): Container => {
-  const _MODULE = new Container({ autobind: true });
-  _MODULE.load(...module);
-  return _MODULE;
-};
