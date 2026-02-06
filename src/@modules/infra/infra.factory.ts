ec9521cfffe0e61f75eca15a0d7c162e4a2bed42
samuel_ricardo <samuelricardoofficial@gmail.com>
2026-02-06 17:56:43 -0300

    [ :syringe: ] | update: factory - infra > presenter (@module)

--- src/@modules/infra/infra.factory.ts
+++ src/@modules/infra/infra.factory.ts
@@ -1,9 +1,11 @@
 import { CONFIG_FACTORY } from "./config/config.factory";
 import { ENGINE_FACTORY } from "./engine/engine.factory";
+import { PRESENTER_FACTORY } from "./presenter/presenter.factory";
 import { SERVER_FACTORY } from "./server/server.factory";
 
 export const INFRA_FACTORY = {
   ENGINE: ENGINE_FACTORY,
   SERVER: SERVER_FACTORY,
   CONFIG: CONFIG_FACTORY,
+  PRESENTER: PRESENTER_FACTORY,
 };
