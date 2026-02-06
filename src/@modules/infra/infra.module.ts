69df7a41feac6f140fee1e46eff3a8a07fd6a2de
samuel_ricardo <samuelricardoofficial@gmail.com>
2026-02-06 17:56:21 -0300

    [ :syringe: ] | update: module - infra > presenter (@module)

--- src/@modules/infra/infra.module.ts
+++ src/@modules/infra/infra.module.ts
@@ -1,5 +1,11 @@
 import { CONFIG_MODULE } from "./config/config.module";
 import { ENGINE_MODULE } from "./engine/engine.module";
+import { PRESENTER_MODULE } from "./presenter/presenter.module";
 import { SERVER_MODULE } from "./server/server.module";
 
-export const INFRA_MODULE = [CONFIG_MODULE, ...ENGINE_MODULE, ...SERVER_MODULE];
+export const INFRA_MODULE = [
+  CONFIG_MODULE,
+  ...ENGINE_MODULE,
+  ...SERVER_MODULE,
+  PRESENTER_MODULE,
+];
