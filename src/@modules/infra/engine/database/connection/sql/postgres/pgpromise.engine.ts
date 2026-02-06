aeba3e51eec6e685cf63e62d11faf963f1e52de0
samuel_ricardo <samuelricardoofficial@gmail.com>
2026-02-06 14:07:37 -0300

    [ :card_file_box: ] | create: adapter - connection > sql [postgres:pgpromise] (@module::infra::engine)

--- src/@modules/infra/engine/database/connection/sql/postgres/pgpromise.engine.ts
+++ src/@modules/infra/engine/database/connection/sql/postgres/pgpromise.engine.ts
@@ -1,10 +1,24 @@
+import { inject, injectable } from "inversify";
 import { SQLDatabaseConnection } from "../sql.connection";
+import pgp, { IDatabase } from "pg-promise";
+import { IClient } from "pg-promise/typescript/pg-subset";
+import { MODULE } from "../../../../../../app.registry";
 
+@injectable()
 export class PgPromiseConnectionAdapter implements SQLDatabaseConnection {
-  query(statement: string, params: any): Promise<any> {
-    throw new Error("Method not implemented.");
+  private connection: IDatabase<object, IClient>;
+
+  constructor(
+    @inject(MODULE.INFRA.CONFIG.ENV.DATABASE.URL)
+    url: string,
+  ) {
+    this.connection = pgp()(url);
   }
-  close(): Promise<void> {
-    throw new Error("Method not implemented.");
+
+  async query(statement: string, params: any): Promise<any> {
+    return this.connection.query(statement, params);
+  }
+  async close(): Promise<void> {
+    await this.connection.$pool.end();
   }
 }
