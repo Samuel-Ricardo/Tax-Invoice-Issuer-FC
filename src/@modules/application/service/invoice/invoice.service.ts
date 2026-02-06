8218b7684c0a8522aa14186b8be4c2e52791a5b6
samuel_ricardo <samuelricardoofficial@gmail.com>
2026-02-06 18:02:45 -0300

    [ :sparkles: ] | create:  service - invoice (@module::application)

---
+++ src/@modules/application/service/invoice/invoice.service.ts
@@ -0,0 +1,17 @@
+import { inject, injectable } from "inversify";
+import { Invoice } from "../../../domain/entity/invoice.entity";
+import { InvoiceService } from "./invoice.interface";
+import { GenerateInvoiceDTO } from "../../../domain/DTO/invoice/generate.dto";
+import { GenerateInvoiceUseCase } from "../../use-case/invoice/generate.interface";
+
+@injectable()
+export class InvoiceServiceImpl implements InvoiceService {
+  constructor(
+    ///@inject(MODULE)
+    private readonly generateInvoice: GenerateInvoiceUseCase,
+  ) {}
+
+  async generate(DTO: GenerateInvoiceDTO): Promise<Invoice> {
+    return this.generateInvoice.execute(DTO);
+  }
+}
