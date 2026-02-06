6672bc376464306e41d2c2a953c26e067b3cf221
samuel_ricardo <samuelricardoofficial@gmail.com>
2026-02-06 14:09:17 -0300

    [ :syringe: ] | update: module - infra > config (@module)

--- src/@modules/infra/infra.module.ts
+++ src/@modules/infra/infra.module.ts
@@ -1,4 +1,5 @@
+import { CONFIG_MODULE } from "./config/config.module";
 import { ENGINE_MODULE } from "./engine/engine.module";
 import { SERVER_MODULE } from "./server/server.module";
 
-export const INFRA_MODULE = [...ENGINE_MODULE, ...SERVER_MODULE];
+export const INFRA_MODULE = [CONFIG_MODULE, ...ENGINE_MODULE, ...SERVER_MODULE];
