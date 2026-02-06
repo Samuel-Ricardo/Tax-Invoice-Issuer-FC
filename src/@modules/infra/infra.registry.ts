2bbd4b95025ec9e75597db04148977b4c9b6fbb4
samuel_ricardo <samuelricardoofficial@gmail.com>
2026-02-06 17:55:50 -0300

    [ :syringe: ] | update: registry - infra > presenter (@module)

--- src/@modules/infra/infra.registry.ts
+++ src/@modules/infra/infra.registry.ts
@@ -1,9 +1,11 @@
 import { CONFIG_REGISTRY } from "./config/config.registry";
 import { ENGINE_REGISTRY } from "./engine/engine.registry";
+import { PRESENTER_REGISTRY } from "./presenter/presenter.registry";
 import { SERVER_REGISTRY } from "./server/server.registry";
 
 export const INFRA_REGISTRY = {
   ENGINE: ENGINE_REGISTRY,
   SERVER: SERVER_REGISTRY,
   CONFIG: CONFIG_REGISTRY,
+  PRESENTER: PRESENTER_REGISTRY,
 };
