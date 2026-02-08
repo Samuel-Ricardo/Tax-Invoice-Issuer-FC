16c525e49dfffd7b8b5f828e9addf28fddbc5310
samuel_ricardo <samuelricardoofficial@gmail.com>
2026-02-08 01:44:38 -0300

    [ :syringe: ] | update: module - app > application (@module)

--- src/@modules/app.module.ts
+++ src/@modules/app.module.ts
@@ -1,4 +1,5 @@
 import { merge } from "../@utils/module/load.util";
+import { APPLICATION_MODULE } from "./application/application.module";
 import { INFRA_MODULE } from "./infra/infra.module";
 
-export const APP_MODULE = merge(INFRA_MODULE);
+export const APP_MODULE = merge([...INFRA_MODULE, ...APPLICATION_MODULE]);
