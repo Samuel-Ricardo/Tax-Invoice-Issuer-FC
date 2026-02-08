759f091b2dba4ea01b96687416e64696fca5ec10
samuel_ricardo <samuelricardoofficial@gmail.com>
2026-02-08 01:31:19 -0300

    [ :sparkles: ] | create:  interface - use-case > invoice [generate] (@module::domain)

--- src/@modules/application/use-case/invoice/generate.interface.ts
+++
@@ -1,6 +0,0 @@
-import { GenerateInvoiceDTO } from "../../../domain/DTO/invoice/generate.dto";
-import { Invoice } from "../../../domain/entity/invoice.entity";
-
-export interface GenerateInvoiceUseCase {
-  execute(DTO: GenerateInvoiceDTO): Invoice;
-}
