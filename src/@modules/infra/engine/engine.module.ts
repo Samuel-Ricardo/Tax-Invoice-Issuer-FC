2803efe9f2893b81ba74c24c6318a810e2e950a7
samuel_ricardo <samuelricardoofficial@gmail.com>
2026-02-09 03:02:13 -0300

    [ :syringe: ] | update: module - engine > database (@module::infra)

--- src/@modules/infra/engine/engine.module.ts
+++ src/@modules/infra/engine/engine.module.ts
@@ -1,3 +1,4 @@
+import { DATABASE_ENGINE_MODULE } from "./database/database.module";
 import { SERVER_ENGINE_MODULE } from "./server/server.module";
 
-export const ENGINE_MODULE = [...SERVER_ENGINE_MODULE];
+export const ENGINE_MODULE = [...SERVER_ENGINE_MODULE, DATABASE_ENGINE_MODULE];
