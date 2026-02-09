d13663b41c4b1a6f59775c023575371d9b58d05a
samuel_ricardo <samuelricardoofficial@gmail.com>
2026-02-09 03:14:30 -0300

    [ :card_file_box: ] | impl: repository - payment [sql] (@module::application)

---
+++ src/@modules/application/repository/payment.repository.ts
@@ -0,0 +1,28 @@
+import { inject, injectable } from "inversify";
+import { PaymentRepository } from "../../domain/repository/payment.repository";
+import { ListPaymentDTO } from "../../domain/DTO/payment/list.dto";
+import Payment from "../../domain/entity/payment.entity";
+import { SQLDatabaseConnection } from "../../infra/engine/database/connection/sql/sql.connection";
+import { MODULE } from "../../app.registry";
+
+@injectable()
+export class PaymentRepositorySQL implements PaymentRepository {
+  constructor(
+    @inject(MODULE.INFRA.ENGINE.DATABASE.SQL.POSTGRES.PGPROMISE)
+    private readonly database: SQLDatabaseConnection,
+  ) {}
+
+  async list({ contrarId }: ListPaymentDTO) {
+    const payments: Payment[] = [];
+    const paymentsDB = await this.database.query(
+      "SELECT * FROM sam.payment WHERE id_contract = $1",
+      [contrarId],
+    );
+
+    for (const p of paymentsDB) {
+      payments.push(new Payment(p.id_payment, p.date, parseFloat(p.amount)));
+    }
+
+    return payments;
+  }
+}
