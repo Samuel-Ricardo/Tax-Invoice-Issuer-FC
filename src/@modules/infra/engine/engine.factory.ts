9256ce111f4ef9cde3148842a80705d8ffff70f7
samuel_ricardo <samuelricardoofficial@gmail.com>
2026-02-09 03:02:34 -0300

    [ :syringe: ] | update: factory - engine > database (@module::infra)

--- src/@modules/infra/engine/engine.factory.ts
+++ src/@modules/infra/engine/engine.factory.ts
@@ -1,5 +1,7 @@
+import { DATABASE_ENGINE_FACTORY } from "./database/database.factory";
 import { SERVER_ENGINE_FACTORY } from "./server/server.factory";
 
 export const ENGINE_FACTORY = {
   SERVER: SERVER_ENGINE_FACTORY,
+  DATABASE: DATABASE_ENGINE_FACTORY,
 };
