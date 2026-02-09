7ff18e45e878cbeb71c41c1e408f4a0e3f03aa64
samuel_ricardo <samuelricardoofficial@gmail.com>
2026-02-09 03:01:47 -0300

    [ :syringe: ] | update: registry - engine > databse (@module::infra)

--- src/@modules/infra/engine/engine.registry.ts
+++ src/@modules/infra/engine/engine.registry.ts
@@ -1,5 +1,7 @@
+import { DATABASE_ENGINE_REGISTRY } from "./database/database.registry";
 import { SERVER_ENGINE_REGISTRY } from "./server/server.registry";
 
 export const ENGINE_REGISTRY = {
   SERVER: SERVER_ENGINE_REGISTRY,
+  DATABASE: DATABASE_ENGINE_REGISTRY,
 };
