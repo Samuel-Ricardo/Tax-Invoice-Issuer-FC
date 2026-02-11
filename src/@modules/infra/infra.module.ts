d331b0c70704d18d0056d6fe85c78e506e2e00fe
samuel_ricardo <samuelricardoofficial@gmail.com>
2026-02-11 04:05:42 -0300

    [ :syringe: ] | update: module - infra > mediator (@module)

--- src/@modules/infra/infra.module.ts
+++ src/@modules/infra/infra.module.ts
@@ -1,5 +1,6 @@
 import { CONFIG_MODULE } from "./config/config.module";
 import { ENGINE_MODULE } from "./engine/engine.module";
+import { MEDIATOR_MODULE } from "./mediator/mediator.module";
 import { PRESENTER_MODULE } from "./presenter/presenter.module";
 import { SERVER_MODULE } from "./server/server.module";
 
@@ -8,4 +9,5 @@ export const INFRA_MODULE = [
   ...ENGINE_MODULE,
   ...SERVER_MODULE,
   PRESENTER_MODULE,
+  MEDIATOR_MODULE,
 ];
