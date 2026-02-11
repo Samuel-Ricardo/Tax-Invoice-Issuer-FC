71f3d99132756a64cecb0cdea3e4035bf0892bd1
samuel_ricardo <samuelricardoofficial@gmail.com>
2026-02-11 02:23:09 -0300

    [ :sparkles: ] | update:  service - invoice > generate (@module::domain)

--- src/@modules/domain/service/invoice/invoice.service.ts
+++ src/@modules/domain/service/invoice/invoice.service.ts
@@ -1,6 +1,7 @@
+import { InvoiceDTO } from "../../DTO/invoice.dto";
 import { GenerateInvoiceDTO } from "../../DTO/invoice/generate.dto";
 import { Invoice } from "../../entity/invoice.entity";
 
 export interface InvoiceService {
-  generate(DTO: GenerateInvoiceDTO): Promise<Invoice>;
+  generate(DTO: InvoiceDTO): Promise<Invoice[]>;
 }
