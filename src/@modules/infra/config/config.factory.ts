da59711a91b8f3dc111e283cf71b6f510aefa872
samuel_ricardo <samuelricardoofficial@gmail.com>
2026-02-11 04:02:19 -0300

    [ :syringe: ] | update: factory - config > events (@module::infra)

--- src/@modules/infra/config/config.factory.ts
+++ src/@modules/infra/config/config.factory.ts
@@ -2,6 +2,7 @@ import { Environment } from "../../../@types/config/env.type";
 import { CONFIG_MODULE } from "./config.module";
 import { CONFIG_REGISTRY } from "./config.registry";
 import { load } from "../../../@utils/module/load.util";
+import { Events } from "../../../@types/config/events.type";
 
 const _MODULE = load(CONFIG_MODULE);
 
@@ -10,6 +11,15 @@ export const CONFIG_FACTORY = {
     IRONMENT: () => _MODULE.get<Environment>(CONFIG_REGISTRY.ENV.IRONMENT),
     DATABASE: {
       URL: () => _MODULE.get<string>(CONFIG_REGISTRY.ENV.DATABASE.URL),
+    },
+  },
+
+  EVENT: {
+    S: () => _MODULE.get<Events>(CONFIG_REGISTRY.EVENT.S),
+
+    INVOICE: {
+      GENERATED: () =>
+        _MODULE.get<string>(CONFIG_REGISTRY.EVENT.INVOICE.GENERATED),
     },
   },
 };
