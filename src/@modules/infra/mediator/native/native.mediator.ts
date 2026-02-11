9e99d614811f377c384b4abaa1a170153a952242
samuel_ricardo <samuelricardoofficial@gmail.com>
2026-02-11 03:19:32 -0300

    [ :sparkles: ] | impl: mediator - native > publish (@module::infra)

--- src/@modules/infra/mediator/native/native.mediator.ts
+++ src/@modules/infra/mediator/native/native.mediator.ts
@@ -4,7 +4,14 @@ import { Mediator } from "../mediator.interface";
 export class NativeMediator implements Mediator {
   observers: Observer[] = [];
 
-  on(event: string, callback: Function): void {
+  on(event: string, callback: Function) {
     this.observers.push({ event, callback });
+  }
+
+  async publish(event: string, data: any) {
+    this.observers.forEach(
+      async (observer) =>
+        observer.event === event && (await observer.callback(data)),
+    );
   }
 }
