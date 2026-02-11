c770d32a614262d967e4c9348033a7ecf75834c5
samuel_ricardo <samuelricardoofficial@gmail.com>
2026-02-11 04:09:59 -0300

    [ :sparkles: ] | impl:  use-case - invoice > generate (@module::application)

--- src/@modules/application/use-case/invoice/generate.use-case.ts
+++ src/@modules/application/use-case/invoice/generate.use-case.ts
@@ -1,12 +1,23 @@
-import { injectable } from "inversify";
+import { inject, injectable } from "inversify";
 import { GenerateInvoiceUseCase } from "../../../domain/use-case/invoice/generate.use-case";
 import { GenerateInvoiceDTO } from "../../../domain/DTO/invoice/generate.dto";
-import { Invoice } from "../../../domain/entity/invoice.entity";
+import { MODULE } from "../../../app.registry";
+import { Mediator } from "../../../infra/mediator/mediator.interface";
 
 @injectable()
 export class GenerateInvoiceUseCaseImpl implements GenerateInvoiceUseCase {
-  constructor() {}
-  execute(DTO: GenerateInvoiceDTO): Invoice {
-    throw new Error("Method not implemented.");
+  constructor(
+    @inject(MODULE.INFRA.MEDIATOR.NATIVE)
+    private readonly mediator: Mediator,
+    @inject(MODULE.INFRA.CONFIG.EVENT.INVOICE.GENERATED)
+    private readonly INVOICE_GENERATED: string,
+  ) {}
+
+  async execute({ contracts, invoice }: GenerateInvoiceDTO) {
+    const result = contracts.flatMap((c) => c.generateInvoices(invoice));
+
+    await this.mediator.publish(this.INVOICE_GENERATED, result);
+
+    return result;
   }
 }
