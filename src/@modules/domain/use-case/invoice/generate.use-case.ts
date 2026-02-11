0c4476935f27c300b4996e58563c58bc53ba586e
samuel_ricardo <samuelricardoofficial@gmail.com>
2026-02-11 02:22:15 -0300

    [ :sparkles: ] | update:  use-case - invoice > generate (@module::domain)

--- src/@modules/domain/use-case/invoice/generate.use-case.ts
+++ src/@modules/domain/use-case/invoice/generate.use-case.ts
@@ -2,5 +2,5 @@ import { GenerateInvoiceDTO } from "../../DTO/invoice/generate.dto";
 import { Invoice } from "../../entity/invoice.entity";
 
 export interface GenerateInvoiceUseCase {
-  execute(DTO: GenerateInvoiceDTO): Invoice;
+  execute(DTO: GenerateInvoiceDTO): Promise<Invoice[]>;
 }
