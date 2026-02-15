67eb2bcacc73974942cda6bef7985cf0008cd07c
samuel_ricardo <samuelricardoofficial@gmail.com>
2026-02-15 02:29:36 -0300

    [ :syringe: ] | update: registry - application > repository (@module)

--- src/@modules/application/application.registry.ts
+++ src/@modules/application/application.registry.ts
@@ -1,5 +1,7 @@
 import { CONTROLLER_REGISTRY } from "./controller/controller.registry";
+import { REPOSITORY_REGISTRY } from "./repository/repository.registry";
 
 export const APPLICATION_REGISTRY = {
   CONTROLLER: CONTROLLER_REGISTRY,
+  REPOSITORY: REPOSITORY_REGISTRY,
 };
