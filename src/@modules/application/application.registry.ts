2e4f63511b2a27af62df9f86e89b84665011c87d
samuel_ricardo <samuelricardoofficial@gmail.com>
2026-02-15 02:31:32 -0300

    [ :syringe: ] | update: registry - application > service (@module)

--- src/@modules/application/application.registry.ts
+++ src/@modules/application/application.registry.ts
@@ -1,7 +1,11 @@
 import { CONTROLLER_REGISTRY } from "./controller/controller.registry";
 import { REPOSITORY_REGISTRY } from "./repository/repository.registry";
+import { SERVICE_REGISTRY } from "./service/service.registry";
+import { SERVICE_REGISTRY } from "./service/service.registry";
 
 export const APPLICATION_REGISTRY = {
   CONTROLLER: CONTROLLER_REGISTRY,
+  SERVICE: SERVICE_REGISTRY,
   REPOSITORY: REPOSITORY_REGISTRY,
+  SERVICE: SERVICE_REGISTRY,
 };
