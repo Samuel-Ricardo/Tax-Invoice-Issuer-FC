ad41525761eb42d0a3189971ee864c8edadfd827
samuel_ricardo <samuelricardoofficial@gmail.com>
2026-02-15 03:41:25 -0300

    [ :syringe: ] | update: factory - application > repository (@module)

--- src/@modules/application/application.factory.ts
+++ src/@modules/application/application.factory.ts
@@ -1,5 +1,8 @@
 import { CONTROLLER_FACTORY } from "./controller/controller.factory";
+import { REPOSITORY_FACTORY } from "./repository/repository.factory";
+import { SERVICE_FACTORY } from "./service/service.factory";
 
 export const APPLICATION_FACTORY = {
   CONTROLLER: CONTROLLER_FACTORY,
+  REPOSITORY: REPOSITORY_FACTORY,
 };
