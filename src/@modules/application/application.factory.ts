4af11371c722a52f0747b6a7f9b9411bbef2ec1e
samuel_ricardo <samuelricardoofficial@gmail.com>
2026-02-15 03:57:17 -0300

    [ :syringe: ] | update: factory - application > use-case (@module)

--- src/@modules/application/application.factory.ts
+++ src/@modules/application/application.factory.ts
@@ -1,9 +1,11 @@
 import { CONTROLLER_FACTORY } from "./controller/controller.factory";
 import { REPOSITORY_FACTORY } from "./repository/repository.factory";
 import { SERVICE_FACTORY } from "./service/service.factory";
+import { USE_CASE_FACTORY } from "./use-case/use-case.factory";
 
 export const APPLICATION_FACTORY = {
   CONTROLLER: CONTROLLER_FACTORY,
   REPOSITORY: REPOSITORY_FACTORY,
   SERVICE: SERVICE_FACTORY,
+  USE_CASE: USE_CASE_FACTORY,
 };
