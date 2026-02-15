c36aefee9ac919530c527c2a4437bcbbff8e4151
samuel_ricardo <samuelricardoofficial@gmail.com>
2026-02-15 03:39:55 -0300

    [ :syringe: ] | update: module - application > service (@module)

--- src/@modules/application/application.module.ts
+++ src/@modules/application/application.module.ts
@@ -1,4 +1,9 @@
 import { CONTROLLER_MODULE } from "./controller/controller.module";
 import { REPOSITORY_MODULE } from "./repository/repository.module";
+import { SERVICE_MODULE } from "./service/service.module";
 
-export const APPLICATION_MODULE = [REPOSITORY_MODULE, CONTROLLER_MODULE];
+export const APPLICATION_MODULE = [
+  REPOSITORY_MODULE,
+  SERVICE_MODULE,
+  CONTROLLER_MODULE,
+];
