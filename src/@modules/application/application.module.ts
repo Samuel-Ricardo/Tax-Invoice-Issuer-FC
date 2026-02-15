b31a3a56cba8cafb9f09074f78fee9e511b3f398
samuel_ricardo <samuelricardoofficial@gmail.com>
2026-02-15 03:54:59 -0300

    [ :syringe: ] | update: module - application > use-case (@module)

--- src/@modules/application/application.module.ts
+++ src/@modules/application/application.module.ts
@@ -1,9 +1,11 @@
 import { CONTROLLER_MODULE } from "./controller/controller.module";
 import { REPOSITORY_MODULE } from "./repository/repository.module";
 import { SERVICE_MODULE } from "./service/service.module";
+import { USE_CASE_MODULE } from "./use-case/use-case.module";
 
 export const APPLICATION_MODULE = [
   REPOSITORY_MODULE,
+  USE_CASE_MODULE,
   SERVICE_MODULE,
   CONTROLLER_MODULE,
 ];
