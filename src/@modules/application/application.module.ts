266dd72330bb1c0a9c18bc8bf5bc08fd22695196
samuel_ricardo <samuelricardoofficial@gmail.com>
2026-02-15 02:31:55 -0300

    [ :syringe: ] | update: module - application > repository (@module)

--- src/@modules/application/application.module.ts
+++ src/@modules/application/application.module.ts
@@ -1,3 +1,4 @@
 import { CONTROLLER_MODULE } from "./controller/controller.module";
+import { REPOSITORY_MODULE } from "./repository/repository.module";
 
-export const APPLICATION_MODULE = [CONTROLLER_MODULE];
+export const APPLICATION_MODULE = [REPOSITORY_MODULE, CONTROLLER_MODULE];
