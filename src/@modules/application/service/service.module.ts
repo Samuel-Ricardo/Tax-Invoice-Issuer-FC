06b715d3e4bf5cb9124a33594b3273e62fd32656
samuel_ricardo <samuelricardoofficial@gmail.com>
2026-02-16 02:44:49 -0300

    [ :syringe: ] | create: module - service > email (@module::application)

--- src/@modules/application/service/service.module.ts
+++ src/@modules/application/service/service.module.ts
@@ -5,4 +5,5 @@ import { EmailServiceImpl } from "./email/email.service";
 
 export const SERVICE_MODULE = new ContainerModule(({ bind }) => {
   bind(SERVICE_REGISTRY.INVOICE).to(InvoiceServiceImpl);
+  bind(SERVICE_REGISTRY.EMAIL).to(EmailServiceImpl);
 });
