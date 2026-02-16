3655103f9a0fdaa88c57deeac4f4b95762b71a2d
samuel_ricardo <samuelricardoofficial@gmail.com>
2026-02-16 02:45:33 -0300

    [ :syringe: ] | create: factory - service (@module::application)

---
+++ src/@modules/application/service/service.factory.ts
@@ -0,0 +1,11 @@
+import { load } from "../../../@utils/module/load.util";
+import { EmailService } from "../../domain/service/email/email.service";
+import { InvoiceService } from "../../domain/service/invoice/invoice.service";
+import { SERVICE_MODULE } from "./service.module";
+import { SERVICE_REGISTRY } from "./service.registry";
+
+const _MODULE = load(SERVICE_MODULE);
+
+export const SERVICE_FACTORY = {
+  INVOICE: () => _MODULE.get<InvoiceService>(SERVICE_REGISTRY.INVOICE),
+};
