8e5b812f6a0881528e607cc774291aae7e75ce1b
samuel_ricardo <samuelricardoofficial@gmail.com>
2026-02-16 02:40:24 -0300

    [ :vertical_traffic_light: ] | create: controller - email (@module::applicaiton)

---
+++ src/@modules/application/controller/email/email.controller.ts
@@ -0,0 +1,32 @@
+import { inject, injectable } from "inversify";
+import { Controller } from "../../../domain/controller/controller.interface";
+import { Mediator } from "../../../infra/mediator/mediator.interface";
+import { MODULE } from "../../../app.registry";
+import { Invoice } from "../../../domain/entity/invoice.entity";
+import { Events } from "../../../../@types/config/events.type";
+import { EmailService } from "../../../domain/service/email/email.service";
+
+@injectable()
+export class EmailController implements Controller {
+  constructor(
+    @inject(MODULE.INFRA.MEDIATOR.NATIVE)
+    private readonly mediator: Mediator,
+
+    @inject(MODULE.INFRA.CONFIG.EVENT.S)
+    private readonly EVENT: Events,
+
+    @inject(MODULE.APPLICATION.SERVICE.EMAIL)
+    private readonly service: EmailService,
+  ) {}
+
+  public startup() {
+    this.mediator.on(
+      this.EVENT.INVOICE.GENERATED,
+      this.sendMailOnInvoiceGenereted,
+    );
+  }
+
+  private async sendMailOnInvoiceGenereted(data: Invoice[]) {
+    await this.service.sendInvoices(data);
+  }
+}
