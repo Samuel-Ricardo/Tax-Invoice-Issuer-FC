0325e0568dc4e7dec82fe85a6cac7b196beb84d3
samuel_ricardo <samuelricardoofficial@gmail.com>
2026-02-16 02:50:08 -0300

    [ :syringe: ] | create: module - use-case (@module::application)

---
+++ src/@modules/application/use-case/use-case.module.ts
@@ -0,0 +1,11 @@
+import { ContainerModule } from "inversify";
+import { USE_CASE_REGISTRY } from "./use-case.registry";
+
+import { GenerateInvoiceUseCaseImpl } from "./invoice/generate.use-case";
+import { ListContractUseCaseImpl } from "./contract/list.use-case";
+
+export const USE_CASE_MODULE = new ContainerModule(({ bind }) => {
+  bind(USE_CASE_REGISTRY.INVOICE.GENERATE).to(GenerateInvoiceUseCaseImpl);
+  bind(USE_CASE_REGISTRY.CONTRACT.LIST).to(ListContractUseCaseImpl);
+  bind(USE_CASE_REGISTRY.EMAIL.SEND.INVOICE).to(GenerateInvoiceUseCaseImpl);
+});
