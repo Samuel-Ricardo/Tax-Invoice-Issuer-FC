77d374909a873b1ee9745ea72dea7f564d4f65c3
samuel_ricardo <samuelricardoofficial@gmail.com>
2026-02-16 02:43:18 -0300

    [ :syringe: ] | create: module - service (@module::application)

---
+++ src/@modules/application/service/service.module.ts
@@ -0,0 +1,8 @@
+import { ContainerModule } from "inversify";
+import { SERVICE_REGISTRY } from "./service.registry";
+import { InvoiceServiceImpl } from "./invoice/invoice.service";
+import { EmailServiceImpl } from "./email/email.service";
+
+export const SERVICE_MODULE = new ContainerModule(({ bind }) => {
+  bind(SERVICE_REGISTRY.INVOICE).to(InvoiceServiceImpl);
+});
