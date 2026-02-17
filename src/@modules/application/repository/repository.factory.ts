e2ffb189e81e5017d577c45c1142a90db4ef1b6c
samuel_ricardo <samuelricardoofficial@gmail.com>
2026-02-17 02:09:56 -0300

    [ :syringe: ] | create: factory - repository (@module::application)

---
+++ src/@modules/application/repository/repository.factory.ts
@@ -0,0 +1,16 @@
+import { load } from "../../../@utils/module/load.util";
+import { ContractRepository } from "../../domain/repository/contract.repository";
+import { PaymentRepository } from "../../domain/repository/payment.repository";
+import { REPOSITORY_MODULE } from "./repository.module";
+import { REPOSITORY_REGISTRY } from "./repository.registry";
+
+const _MODULE = load(REPOSITORY_MODULE);
+
+export const REPOSITORY_FACTORY = {
+  SQL: {
+    CONTRACT: () =>
+      _MODULE.get<ContractRepository>(REPOSITORY_REGISTRY.SQL.CONTRACT),
+    PAYMENT: () =>
+      _MODULE.get<PaymentRepository>(REPOSITORY_REGISTRY.SQL.PAYMENT),
+  },
+};
