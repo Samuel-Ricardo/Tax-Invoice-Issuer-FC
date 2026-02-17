d755e4dbc11f33691f1e4dfc05e92e4c159374cf
samuel_ricardo <samuelricardoofficial@gmail.com>
2026-02-17 02:08:27 -0300

    [ :card_file_box: ] | impl: repository - contract [sql] (@module::application)

---
+++ src/@modules/application/repository/sql/contract.repository.ts
@@ -0,0 +1,35 @@
+import { inject, injectable } from "inversify";
+import { ContractRepository } from "../../../domain/repository/contract.repository";
+import { SQLDatabaseConnection } from "../../../infra/engine/database/connection/sql/sql.connection";
+import { MODULE } from "../../../app.registry";
+import { Contract } from "../../../domain/entity/contract.entity";
+
+@injectable()
+export class ContractRepositorySQL implements ContractRepository {
+  constructor(
+    @inject(MODULE.INFRA.ENGINE.DATABASE.SQL.POSTGRES.PGPROMISE)
+    private readonly database: SQLDatabaseConnection,
+  ) {}
+
+  async list() {
+    const contracts: Contract[] = [];
+    const contractsDB = await this.database.query(
+      "SELECT * FROM sam.contract",
+      [],
+    );
+
+    for (const c of contractsDB) {
+      contracts.push(
+        new Contract(
+          c.id_contract,
+          c.description,
+          parseFloat(c.amount),
+          c.periods,
+          c.date,
+        ),
+      );
+    }
+
+    return contracts;
+  }
+}
