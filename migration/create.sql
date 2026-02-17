45d0586292e252ecf6946527725eabb14e67ed7d
samuel_ricardo <samuelricardoofficial@gmail.com>
2026-02-17 04:32:58 -0300

    [ :card_file_box: ] | create: migration - table > payment (@app::root)

--- migration/create.sql
+++ migration/create.sql
@@ -1,2 +1,20 @@
-DROPschema samcascade;
-CREATEschema sam;
+drop schema sam cascade;
+
+create schema sam;
+
+create table sam.contract (
+  id_contract uuid not null default uuid_generate_v4() primary key,
+  description text, 
+  amount numeric,
+  periods integer,
+  date timestamp
+);
+
+create table sam.payment (
+  id_payment uuid not null default uuid_generate_v4() primary key,
+  id_contract uuid references sam.contract(id_contract),
+  amount numeric,
+  date timestamp
+);
+
+
