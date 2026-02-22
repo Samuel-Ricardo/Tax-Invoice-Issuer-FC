a98c04c0a66cbbf483ec7e4dcaae3e4d8f7d7525
samuel_ricardo <samuelricardoofficial@gmail.com>
2026-02-22 00:01:46 -0300

    [ :rotating_light: ] | fix: service - invoice > modules & imports (project::lint)

--- src/@modules/application/service/invoice/invoice.service.ts
+++ src/@modules/application/service/invoice/invoice.service.ts
@@ -1,16 +1,17 @@
 import { inject, injectable } from "inversify";
 import { Invoice } from "../../../domain/entity/invoice.entity";
 import { InvoiceService } from "../../../domain/service/invoice/invoice.service";
-import { GenerateInvoiceDTO } from "../../../domain/DTO/invoice/generate.dto";
 import { GenerateInvoiceUseCase } from "../../../domain/use-case/invoice/generate.use-case";
 import { ListContractUseCase } from "../../../domain/use-case/contract/list.use-case";
 import { InvoiceDTO } from "../../../domain/DTO/invoice.dto";
+import { MODULE } from "../../../app.registry";
 
 @injectable()
 export class InvoiceServiceImpl implements InvoiceService {
   constructor(
-    ///@inject(MODULE)
+    @inject(MODULE.APPLICATION.USE_CASE.INVOICE.GENERATE)
     private readonly generateInvoice: GenerateInvoiceUseCase,
+    @inject(MODULE.APPLICATION.USE_CASE.CONTRACT.LIST)
     private readonly listContract: ListContractUseCase,
   ) {}
 
