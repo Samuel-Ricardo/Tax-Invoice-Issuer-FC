e47b9f4f684d2137be68680f7c6efd07feb7d80e
samuel_ricardo <samuelricardoofficial@gmail.com>
2026-02-22 00:04:39 -0300

    [ :rotating_light: ] | fix: contract - list > modules & names (@module::application::use-case)

--- src/@modules/application/use-case/contract/list.use-case.ts
+++ src/@modules/application/use-case/contract/list.use-case.ts
@@ -1,22 +1,25 @@
-import { injectable } from "inversify";
+import { inject, injectable } from "inversify";
 import { ListContractUseCase } from "../../../domain/use-case/contract/list.use-case";
 import { PaymentRepository } from "../../../domain/repository/payment.repository";
 import { ContractRepository } from "../../../domain/repository/contract.repository";
 
 import { Contract } from "../../../domain/entity/contract.entity";
 import Payment from "../../../domain/entity/payment.entity";
+import { MODULE } from "../../../app.registry";
 
 @injectable()
 export class ListContractUseCaseImpl implements ListContractUseCase {
   constructor(
-    //    @inject(MODULE.APPLICATION.REPOSITORY)
+    @inject(MODULE.APPLICATION.REPOSITORY.SQL.CONTRACT)
     private readonly reposiotry: ContractRepository,
-    //    @inject(MODULE.APPLICATION.REPOSITORY)
+    @inject(MODULE.APPLICATION.REPOSITORY.SQL.PAYMENT)
     private readonly payment: PaymentRepository,
   ) {}
 
   async execute() {
-    return this.reposiotry.list().then(this.attachPayment);
+    return this.reposiotry
+      .list()
+      .then((contracts) => this.attachPayment(contracts));
   }
 
   private async attachPayment(contracts: Contract[]) {
