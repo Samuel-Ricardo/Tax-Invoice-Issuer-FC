f8e2fe9d618d8921036b31515d733e863b8807ec
samuel_ricardo <samuelricardoofficial@gmail.com>
2026-02-23 04:34:38 -0300

    [ :syringe: ] | fix: factory - server > http [load-conflict] (@module::infra)

--- src/@modules/infra/server/http/http.factory.ts
+++ src/@modules/infra/server/http/http.factory.ts
@@ -2,8 +2,9 @@ import { HTTP_SERVER_MODULE } from "./http.module";
 import { merge } from "../../../../@utils/module/load.util";
 import { HttpServer } from "./http.server";
 import { HTTP_SERVER_REGISTRY } from "./http.registry";
+import { ENGINE_MODULE } from "../../engine/engine.module";
 
-const _MODULE = merge(HTTP_SERVER_MODULE);
+const _MODULE = merge([...ENGINE_MODULE, ...HTTP_SERVER_MODULE]);
 
 export const HTTP_SERVER_FACTORY = {
   EXPRESS: () => _MODULE.get<HttpServer>(HTTP_SERVER_REGISTRY.EXPRESS),
