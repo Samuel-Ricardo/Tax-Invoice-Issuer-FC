d2d82689dfe7f9ebe5094711fbfde50ac2f6a3fd
samuel_ricardo <samuelricardoofficial@gmail.com>
2026-02-23 04:46:07 -0300

    [ :syringe: ] | update: module - controller > email (@module::application)

--- src/@modules/application/controller/controller.module.ts
+++ src/@modules/application/controller/controller.module.ts
@@ -2,7 +2,9 @@ import { ContainerModule } from "inversify";
 import { Controller } from "../../domain/controller/controller.interface";
 import { InvoiceController } from "./invoice/invoice.controller";
 import { CONTROLLER_REGISTRY } from "./controller.registry";
+import { EmailController } from "./email/email.controller";
 
 export const CONTROLLER_MODULE = new ContainerModule(({ bind }) => {
   bind<Controller>(CONTROLLER_REGISTRY.INVOICE).to(InvoiceController);
+  bind<Controller>(CONTROLLER_REGISTRY.EMAIL).to(EmailController);
 });
