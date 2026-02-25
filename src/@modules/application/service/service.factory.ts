e908f1a78ed411a3f9b6a8fc1ccfced1359dd4bd
samuel_ricardo <samuelricardoofficial@gmail.com>
2026-02-25 01:03:27 -0300

    [ :recycle: ] | refact: rename - function > merge => loads [service] (@module::utils)

--- src/@modules/application/service/service.factory.ts
+++ src/@modules/application/service/service.factory.ts
@@ -1,10 +1,11 @@
-import { load } from "../../../@utils/module/load.util";
+import { loads } from "../../../@utils/module/load.util";
 import { EmailService } from "../../domain/service/email/email.service";
 import { InvoiceService } from "../../domain/service/invoice/invoice.service";
+import { USE_CASE_MODULE } from "../use-case/use-case.module";
 import { SERVICE_MODULE } from "./service.module";
 import { SERVICE_REGISTRY } from "./service.registry";
 
-const _MODULE = load(SERVICE_MODULE);
+const _MODULE = loads([USE_CASE_MODULE, SERVICE_MODULE]);
 
 export const SERVICE_FACTORY = {
   INVOICE: () => _MODULE.get<InvoiceService>(SERVICE_REGISTRY.INVOICE),
