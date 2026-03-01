4e5f20211e2df1af62ac7a7549d0457d63062844
samuel_ricardo <samuelricardoofficial@gmail.com>
2026-03-01 03:55:47 -0300

    [ :syringe: ] | update: module - service > imports (@module::application)

--- src/@modules/application/service/service.module.ts
+++ src/@modules/application/service/service.module.ts
@@ -2,8 +2,12 @@ import { ContainerModule } from "inversify";
 import { SERVICE_REGISTRY } from "./service.registry";
 import { InvoiceServiceImpl } from "./invoice/invoice.service";
 import { EmailServiceImpl } from "./email/email.service";
+import { USE_CASE_MODULE } from "../use-case/use-case.module";
 
-export const SERVICE_MODULE = new ContainerModule(({ bind }) => {
-  bind(SERVICE_REGISTRY.INVOICE).to(InvoiceServiceImpl);
-  bind(SERVICE_REGISTRY.EMAIL).to(EmailServiceImpl);
-});
+export const SERVICE_MODULE: ContainerModule[] = [
+  USE_CASE_MODULE,
+  new ContainerModule(({ bind }) => {
+    bind(SERVICE_REGISTRY.INVOICE).to(InvoiceServiceImpl);
+    bind(SERVICE_REGISTRY.EMAIL).to(EmailServiceImpl);
+  }),
+];
