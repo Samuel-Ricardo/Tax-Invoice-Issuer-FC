2e2f0e3bd79faf7a5e3fcd8ba7f64495389bfc2f
samuel_ricardo <samuelricardoofficial@gmail.com>
2026-03-01 03:31:15 -0300

    [ :syringe: ] | update: module - controller > imports (@module::application)

--- src/@modules/application/controller/controller.module.ts
+++ src/@modules/application/controller/controller.module.ts
@@ -3,8 +3,14 @@ import { Controller } from "../../domain/controller/controller.interface";
 import { InvoiceController } from "./invoice/invoice.controller";
 import { CONTROLLER_REGISTRY } from "./controller.registry";
 import { EmailController } from "./email/email.controller";
+import { SERVICE_MODULE } from "../service/service.module";
+import { INFRA_MODULE } from "../../infra/infra.module";
 
-export const CONTROLLER_MODULE = new ContainerModule(({ bind }) => {
-  bind<Controller>(CONTROLLER_REGISTRY.INVOICE).to(InvoiceController);
-  bind<Controller>(CONTROLLER_REGISTRY.EMAIL).to(EmailController);
-});
+export const CONTROLLER_MODULE = [
+  ...INFRA_MODULE,
+  ...SERVICE_MODULE,
+  new ContainerModule(({ bind }) => {
+    bind<Controller>(CONTROLLER_REGISTRY.INVOICE).to(InvoiceController);
+    bind<Controller>(CONTROLLER_REGISTRY.EMAIL).to(EmailController);
+  }),
+];
