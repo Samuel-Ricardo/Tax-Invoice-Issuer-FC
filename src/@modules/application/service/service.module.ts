8b1a9b63b61bd4b106285fe057b35bd865a87a52
samuel_ricardo <samuelricardoofficial@gmail.com>
2026-03-01 04:02:38 -0300

    [ :syringe: ] | update: module - service > imports (@module::application)

--- src/@modules/application/service/service.module.ts
+++ src/@modules/application/service/service.module.ts
@@ -5,7 +5,7 @@ import { EmailServiceImpl } from "./email/email.service";
 import { USE_CASE_MODULE } from "../use-case/use-case.module";
 
 export const SERVICE_MODULE: ContainerModule[] = [
-  USE_CASE_MODULE,
+  ...USE_CASE_MODULE,
   new ContainerModule(({ bind }) => {
     bind(SERVICE_REGISTRY.INVOICE).to(InvoiceServiceImpl);
     bind(SERVICE_REGISTRY.EMAIL).to(EmailServiceImpl);
