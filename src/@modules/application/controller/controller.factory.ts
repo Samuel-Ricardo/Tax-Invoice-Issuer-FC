1513644ba7f61b9a5024a2911ae4ff780ef94e99
samuel_ricardo <samuelricardoofficial@gmail.com>
2026-03-01 03:51:46 -0300

    [ :syringe: ] | update: factory - controller > imports (@module::application)

--- src/@modules/application/controller/controller.factory.ts
+++ src/@modules/application/controller/controller.factory.ts
@@ -1,11 +1,10 @@
 import { loads } from "../../../@utils/module/load.util";
 import { Controller } from "../../domain/controller/controller.interface";
-import { INFRA_MODULE } from "../../infra/infra.module";
-import { SERVICE_MODULE } from "../service/service.module";
+
 import { CONTROLLER_MODULE } from "./controller.module";
 import { CONTROLLER_REGISTRY } from "./controller.registry";
 
-const _MODULE = loads([...INFRA_MODULE, SERVICE_MODULE, CONTROLLER_MODULE]);
+const _MODULE = loads(CONTROLLER_MODULE);
 
 export const CONTROLLER_FACTORY = {
   INVOICE: () => _MODULE.get<Controller>(CONTROLLER_REGISTRY.INVOICE),
