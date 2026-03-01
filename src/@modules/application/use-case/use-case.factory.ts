442b752da0460da724b250327ac04c475fd051a4
samuel_ricardo <samuelricardoofficial@gmail.com>
2026-03-01 04:02:23 -0300

    [ :syringe: ] | update: factory - use-case > imports (@module::application)

--- src/@modules/application/use-case/use-case.factory.ts
+++ src/@modules/application/use-case/use-case.factory.ts
@@ -2,12 +2,10 @@ import { loads } from "../../../@utils/module/load.util";
 import { ListContractUseCase } from "../../domain/use-case/contract/list.use-case";
 import { SendInvoiceEmailUseCase } from "../../domain/use-case/email/send/invoice.use-case";
 import { GenerateInvoiceUseCase } from "../../domain/use-case/invoice/generate.use-case";
-import { INFRA_MODULE } from "../../infra/infra.module";
-import { REPOSITORY_MODULE } from "../repository/repository.module";
 import { USE_CASE_MODULE } from "./use-case.module";
 import { USE_CASE_REGISTRY } from "./use-case.registry";
 
-const _MODULE = loads([...INFRA_MODULE, REPOSITORY_MODULE, USE_CASE_MODULE]);
+const _MODULE = loads(USE_CASE_MODULE);
 
 export const USE_CASE_FACTORY = {
   INVOICE: {
