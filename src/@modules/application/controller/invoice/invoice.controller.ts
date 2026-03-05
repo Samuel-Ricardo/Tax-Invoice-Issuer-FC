31f92760bb26d330f5d2c6d09c19949bd2b5d9ea
samuel_ricardo <samuelricardoofficial@gmail.com>
2026-03-05 02:45:42 -0300

    [ :recycle: ] | refact: controller - invoice > setup (@module::application)

--- src/@modules/application/controller/invoice/invoice.controller.ts
+++ src/@modules/application/controller/invoice/invoice.controller.ts
@@ -4,6 +4,8 @@ import { HttpServer } from "../../../infra/server/http/http.server";
 import { MODULE } from "../../../app.registry";
 import { Controller } from "../../../domain/controller/controller.interface";
 import { Presenter } from "../../../infra/presenter/presenter.interface";
+import { DataLogger } from "../../../../@decorators/log/data.decorator";
+import { InvoiceDTO } from "../../../domain/DTO/invoice.dto";
 
 @injectable()
 export class InvoiceController implements Controller {
@@ -17,16 +19,28 @@ export class InvoiceController implements Controller {
   ) {}
 
   public async setup() {
-    this.generateInvoice();
+    this.setupRoutes();
   }
 
   public async start() {
     this.server.listen();
   }
 
-  private generateInvoice() {
-    this.server.on("post", "/invoice", async (_params, body, _headers) =>
-      this.service.generate(body).then(this.presenter.present),
-    );
+  private setupRoutes() {
+    this.server.on("post", "/invoice", this.generateInvoice.bind(this));
+    this.server.on("get", "/", async () => ({
+      hello: "world",
+    }));
+  }
+
+  //TODO: VALIDATION WITH ZOD & ERROR HANDLING DECORATORS
+  @DataLogger()
+  private async generateInvoice(_params, body: InvoiceDTO, _headers) {
+    if (!body || !body.month || !body.year || !body.type) {
+      throw new Error(
+        "Invalid request body. Required fields: month, year, type",
+      );
+    }
+    return this.service.generate(body).then(this.presenter.present);
   }
 }
