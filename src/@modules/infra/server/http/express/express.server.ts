3e7f2fd0cff3c0a41afb7da2acc6dc994a8200ce
samuel_ricardo <samuelricardoofficial@gmail.com>
2026-03-05 03:51:31 -0300

    [ :sparkles: ] | update: adapter - server [http] > express [setup] (@module::infra)

--- src/@modules/infra/server/http/express/express.server.ts
+++ src/@modules/infra/server/http/express/express.server.ts
@@ -8,17 +8,40 @@ import {
   LoggableAsync,
 } from "../../../../../@decorators/async/logger.decorator";
 import { ServerCallback } from "../../../../../@types/http/callback.type";
+import { Cors } from "../../../../../@types/engine/server/http/cors.type";
+import { JsonParser } from "../../../../../@types/engine/server/http/parser/json.type";
 
 @AsyncLogger()
 @injectable()
 export class ExpressServerAdapter extends LoggableAsync implements HttpServer {
-  @inject(MODULE.INFRA.ENGINE.SERVER.HTTP.EXPRESS._)
-  private readonly server: TExpress;
+  constructor(
+    @inject(MODULE.INFRA.ENGINE.SERVER.HTTP.EXPRESS._)
+    private readonly server: TExpress,
+    @inject(MODULE.INFRA.ENGINE.SERVER.HTTP.EXPRESS.PARSER.JSON)
+    private readonly parser: JsonParser,
+    @inject(MODULE.INFRA.ENGINE.SERVER.HTTP.EXPRESS.CORS)
+    private readonly cors: Cors,
+  ) {
+    super();
+    this.setup();
+  }
 
   on(method: HttpMethod, url: string, callback: ServerCallback): void {
     this.server[method](url, async function (req, res) {
-      const output = await callback(req.params, req.body, req.headers);
-      res.json(output);
+      try {
+        const output = await callback(req.params, req.body, req.headers);
+        res.json(output);
+      } catch (error: any) {
+        res.status(400).json({
+          error: error.message || "An error occurred",
+          status: 400,
+        });
+      }
+    });
+
+    this.info({
+      context: "SERVER",
+      message: `Registered ${method.toUpperCase()} ${url}`,
     });
   }
 
@@ -27,6 +50,13 @@ export class ExpressServerAdapter extends LoggableAsync implements HttpServer {
       context: "SERVER",
       message: `Listening on http://localhost:${p}`,
     });
+
     this.server.listen(p);
+  }
+
+  private setup(): void {
+    this.server.use(this.cors);
+    this.server.use(this.parser);
+    this.info({ context: "SERVER", message: "Setup Express Server" });
   }
 }
