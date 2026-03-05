a3bcfdca2c5905c51d007d4facea65360136f7eb
samuel_ricardo <samuelricardoofficial@gmail.com>
2026-03-05 03:49:59 -0300

    [ :loud_sound: ] | log: presenter - json (@module::infra)

--- src/@modules/infra/presenter/json/json.presenter.ts
+++ src/@modules/infra/presenter/json/json.presenter.ts
@@ -1,6 +1,8 @@
+import { DataLogger } from "../../../../@decorators/log/data.decorator";
 import { Presenter } from "../presenter.interface";
 
 export class JsonPresenter implements Presenter {
+  @DataLogger("PRESENTER")
   present(data: any) {
     return JSON.stringify(data);
   }
