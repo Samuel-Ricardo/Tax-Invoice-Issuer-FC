a1e7fa269466a9efade911c0388232e58e4c1dd1
samuel_ricardo <samuelricardoofficial@gmail.com>
2026-03-05 02:46:47 -0300

    [ :syringe: ] | update: module - repository > imports (@module::application)

--- src/@modules/application/repository/repository.module.ts
+++ src/@modules/application/repository/repository.module.ts
@@ -2,8 +2,12 @@ import { ContainerModule } from "inversify";
 import { REPOSITORY_REGISTRY } from "./repository.registry";
 import { ContractRepositorySQL } from "./sql/contract.repository";
 import { PaymentRepositorySQL } from "./sql/payment.repository";
+import { INFRA_MODULE } from "../../infra/infra.module";
 
-export const REPOSITORY_MODULE = new ContainerModule(({ bind }) => {
-  bind(REPOSITORY_REGISTRY.SQL.CONTRACT).to(ContractRepositorySQL);
-  bind(REPOSITORY_REGISTRY.SQL.PAYMENT).to(PaymentRepositorySQL);
-});
+export const REPOSITORY_MODULE = [
+  ...INFRA_MODULE,
+  new ContainerModule(({ bind }) => {
+    bind(REPOSITORY_REGISTRY.SQL.CONTRACT).to(ContractRepositorySQL);
+    bind(REPOSITORY_REGISTRY.SQL.PAYMENT).to(PaymentRepositorySQL);
+  }),
+];
