159b34bdc26f204682b1d30655f87f8686dc5224
samuel_ricardo <samuelricardoofficial@gmail.com>
2026-03-05 03:47:43 -0300

    [ :syringe: ] | update: registry - engine > databse [scope:singleton] (@module::infra)

--- src/@modules/infra/engine/database/database.module.ts
+++ src/@modules/infra/engine/database/database.module.ts
@@ -4,7 +4,7 @@ import { DATABASE_ENGINE_REGISTRY } from "./database.registry";
 import { PgPromiseConnectionAdapter } from "./connection/sql/postgres/pgpromise.engine";
 
 export const DATABASE_ENGINE_MODULE = new ContainerModule(({ bind }) => {
-  bind<SQLDatabaseConnection>(
-    DATABASE_ENGINE_REGISTRY.SQL.POSTGRES.PGPROMISE,
-  ).to(PgPromiseConnectionAdapter);
+  bind<SQLDatabaseConnection>(DATABASE_ENGINE_REGISTRY.SQL.POSTGRES.PGPROMISE)
+    .to(PgPromiseConnectionAdapter)
+    .inSingletonScope();
 });
