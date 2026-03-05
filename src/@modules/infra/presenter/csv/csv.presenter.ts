1491d84d9c371e5f086aac17362f43ae5e8883ac
samuel_ricardo <samuelricardoofficial@gmail.com>
2026-03-05 03:50:15 -0300

    [ :loud_sound: ] | log: presenter - csv (@module::infra)

--- src/@modules/infra/presenter/csv/csv.presenter.ts
+++ src/@modules/infra/presenter/csv/csv.presenter.ts
@@ -1,7 +1,9 @@
 import moment from "moment";
 import { Presenter } from "../presenter.interface";
+import { DataLogger } from "../../../../@decorators/log/data.decorator";
 
 export default class CsvPresenter implements Presenter {
+  @DataLogger("PRESENTER")
   present(data: any[]) {
     return data
       .flatMap((d) =>
