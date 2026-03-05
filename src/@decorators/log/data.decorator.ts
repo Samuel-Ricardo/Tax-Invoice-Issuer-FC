d2df75d25b3fb621c5d4694c9fce529bdb65f9ef
samuel_ricardo <samuelricardoofficial@gmail.com>
2026-03-05 02:39:05 -0300

    [ :bug: ] | fix: decorator - log > pass params (@app::decorator)

--- src/@decorators/log/data.decorator.ts
+++ src/@decorators/log/data.decorator.ts
@@ -1,27 +1,27 @@
-export function InputLogger(): MethodDecorator {
+export function InputLogger(label?: string): MethodDecorator {
   return (_target, _propertyKey, descriptor) => {
     const original = descriptor.value as (
       ...input: any[]
     ) => Promise<any> | any;
 
     descriptor.value = async function (...input: any[]) {
-      console.log({ input });
-      return await original.call(this, input);
+      console.log({ label, input });
+      return await original.apply(this, input);
     } as any;
 
     return descriptor;
   };
 }
 
-export function OutputLogger(): MethodDecorator {
+export function OutputLogger(label?: string): MethodDecorator {
   return (_target, _propertyKey, descriptor) => {
     const original = descriptor.value as (
       ...input: any[]
     ) => Promise<any> | any;
 
     descriptor.value = async function (...input: any[]) {
-      const output = await original.call(this, input);
-      console.log({ output });
+      const output = await original.apply(this, input);
+      console.log({ label, output });
       return output;
     } as any;
 
@@ -29,15 +29,16 @@ export function OutputLogger(): MethodDecorator {
   };
 }
 
-export function DataLogger(): MethodDecorator {
+//TODO: CREATE LOGGER PARAMS DTO
+export function DataLogger(label?: string): MethodDecorator {
   return (_target, _propertyKey, descriptor: PropertyDescriptor) => {
     const original = descriptor.value as (...args: any[]) => any;
 
     descriptor.value = async function (...args: any[]) {
-      console.log({ args });
+      console.log({ label, args });
 
       const result = await original.apply(this, args);
-      console.log({ result });
+      console.log({ label, result });
 
       return result;
     };
