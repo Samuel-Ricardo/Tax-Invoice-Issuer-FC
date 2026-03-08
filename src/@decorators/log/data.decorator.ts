02f0a61e8a2625b3d9ef8e28bd1b0163c9d516ac
samuel_ricardo <samuelricardoofficial@gmail.com>
2026-03-08 02:17:31 -0300

    [ :loud_sound: ] | update: decorator - log > structure [output] (@module::decorator)

--- src/@decorators/log/data.decorator.ts
+++ src/@decorators/log/data.decorator.ts
@@ -20,3 +20,25 @@ export function InputLogger(
     return descriptor;
   };
 }
+
+export function OutputLogger(
+  config?: LogInput,
+  ...data: any[]
+): MethodDecorator {
+  return (_target, _propertyKey, descriptor) => {
+    const original = descriptor.value as (
+      ...input: any[]
+    ) => Promise<any> | any;
+
+    descriptor.value = async function (...input: any[]) {
+      const output = await original.apply(this, input);
+      const context = config?.context || "DATA::OUTPUT";
+      const message = config.message;
+
+      console.info(`[${context}] | ${message}`, { output }, ...data);
+      return output;
+    } as any;
+
+    return descriptor;
+  };
+}
