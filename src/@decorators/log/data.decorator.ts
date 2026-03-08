22a0f4fdb25afac7646377214bb23cd8f855d7e8
samuel_ricardo <samuelricardoofficial@gmail.com>
2026-03-08 02:16:42 -0300

    [ :loud_sound: ] | update: decorator - log > structure (@module::decorator)

--- src/@decorators/log/data.decorator.ts
+++ src/@decorators/log/data.decorator.ts
@@ -1,46 +1,22 @@
-export function InputLogger(label?: string): MethodDecorator {
+import { LogInput } from "../../@types/decorator/log/input.type";
+
+export function InputLogger(
+  config?: LogInput,
+  ...data: any[]
+): MethodDecorator {
   return (_target, _propertyKey, descriptor) => {
     const original = descriptor.value as (
       ...input: any[]
     ) => Promise<any> | any;
 
     descriptor.value = async function (...input: any[]) {
-      console.log({ label, input });
+      const context = config?.context || "DATA::INPUT";
+      const message = config.message;
+
+      console.info(`[${context}] | ${message}`, { input }, ...data);
       return await original.apply(this, input);
     } as any;
 
     return descriptor;
-  };
-}
-
-export function OutputLogger(label?: string): MethodDecorator {
-  return (_target, _propertyKey, descriptor) => {
-    const original = descriptor.value as (
-      ...input: any[]
-    ) => Promise<any> | any;
-
-    descriptor.value = async function (...input: any[]) {
-      const output = await original.apply(this, input);
-      console.log({ label, output });
-      return output;
-    } as any;
-
-    return descriptor;
-  };
-}
-
-//TODO: CREATE LOGGER PARAMS DTO
-export function DataLogger(label?: string): MethodDecorator {
-  return (_target, _propertyKey, descriptor: PropertyDescriptor) => {
-    const original = descriptor.value as (...args: any[]) => any;
-
-    descriptor.value = async function (...args: any[]) {
-      console.log({ label, args });
-
-      const result = await original.apply(this, args);
-      console.log({ label, result });
-
-      return result;
-    };
   };
 }
