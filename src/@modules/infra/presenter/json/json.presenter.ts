a19586af23ae9c9e8f6a5edfc84b01e7d128f930
samuel_ricardo <samuelricardoofficial@gmail.com>
2026-03-16 01:02:11 -0300

    [ :loud_sound: ] | log: presenter - json > params (@module::infra)

--- src/@modules/infra/presenter/json/json.presenter.ts
+++ src/@modules/infra/presenter/json/json.presenter.ts
@@ -2,7 +2,7 @@ import { DataLogger } from "../../../../@decorators/log/data.decorator";
 import { Presenter } from "../presenter.interface";
 
 export class JsonPresenter implements Presenter {
-  @DataLogger("PRESENTER")
+  @DataLogger({ context: "PRESENTER", message: "JSON" })
   present(data: any) {
     return JSON.stringify(data);
   }
