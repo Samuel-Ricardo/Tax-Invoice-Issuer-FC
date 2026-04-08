814709dfc8e54201cd28a657928ba31580da3fe1
samuel_ricardo <samuelricardoofficial@gmail.com>
2026-04-08 01:35:23 -0300

    [ :syringe: ] | update: registry - engine > validation (@module::infra)

--- src/@modules/infra/engine/engine.registry.ts
+++ src/@modules/infra/engine/engine.registry.ts
@@ -1,7 +1,9 @@
 import { DATABASE_ENGINE_REGISTRY } from "./database/database.registry";
 import { SERVER_ENGINE_REGISTRY } from "./server/server.registry";
+import { ENGINE_VALIDATION_REGISTRY } from "./validation/validation.registry";
 
 export const ENGINE_REGISTRY = {
   SERVER: SERVER_ENGINE_REGISTRY,
   DATABASE: DATABASE_ENGINE_REGISTRY,
+  VALIDATION: ENGINE_VALIDATION_REGISTRY,
 };
