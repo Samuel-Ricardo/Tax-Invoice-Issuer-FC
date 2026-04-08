89a158b1d95404465ee11f74cf40e36cf8c3d2b3
samuel_ricardo <samuelricardoofficial@gmail.com>
2026-04-08 01:37:36 -0300

    [ :syringe: ] | update: module - engine > validation (@module::infra)

--- src/@modules/infra/engine/engine.factory.ts
+++ src/@modules/infra/engine/engine.factory.ts
@@ -1,7 +1,9 @@
 import { DATABASE_ENGINE_FACTORY } from "./database/database.factory";
 import { SERVER_ENGINE_FACTORY } from "./server/server.factory";
+import { ENGINE_VALIDATION_FACTORY } from "./validation/validation.factory";
 
 export const ENGINE_FACTORY = {
   SERVER: SERVER_ENGINE_FACTORY,
   DATABASE: DATABASE_ENGINE_FACTORY,
+  VALIDATION: ENGINE_VALIDATION_FACTORY,
 };
