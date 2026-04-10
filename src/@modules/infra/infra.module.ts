f718b328b74e7b72ec97a8782099674570759cb3
samuel_ricardo <samuelricardoofficial@gmail.com>
2026-04-10 00:47:17 -0300

    [ :syringe: ] | update: module - infra > validator (@module)

--- src/@modules/infra/infra.module.ts
+++ src/@modules/infra/infra.module.ts
@@ -3,6 +3,7 @@ import { ENGINE_MODULE } from "./engine/engine.module";
 import { MEDIATOR_MODULE } from "./mediator/mediator.module";
 import { PRESENTER_MODULE } from "./presenter/presenter.module";
 import { SERVER_MODULE } from "./server/server.module";
+import { VALIDATOR_MODULE } from "./validator/validator.module";
 
 export const INFRA_MODULE = [
   CONFIG_MODULE,
@@ -10,4 +11,5 @@ export const INFRA_MODULE = [
   ...SERVER_MODULE,
   PRESENTER_MODULE,
   MEDIATOR_MODULE,
+  ...VALIDATOR_MODULE,
 ];
