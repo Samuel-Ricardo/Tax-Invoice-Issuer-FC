bbf30e203c7c3cd7f0ce6d2169a3f159448cfee1
samuel_ricardo <samuelricardoofficial@gmail.com>
2026-04-10 00:45:43 -0300

    [ :syringe: ] | update: registry - infra > validator (@module)

--- src/@modules/infra/infra.registry.ts
+++ src/@modules/infra/infra.registry.ts
@@ -3,6 +3,7 @@ import { ENGINE_REGISTRY } from "./engine/engine.registry";
 import { MEDIATOR_REGISTRY } from "./mediator/mediator.registry";
 import { PRESENTER_REGISTRY } from "./presenter/presenter.registry";
 import { SERVER_REGISTRY } from "./server/server.registry";
+import { VALIDATOR_REGISTRY } from "./validator/validator.registry";
 
 export const INFRA_REGISTRY = {
   ENGINE: ENGINE_REGISTRY,
@@ -10,4 +11,5 @@ export const INFRA_REGISTRY = {
   CONFIG: CONFIG_REGISTRY,
   PRESENTER: PRESENTER_REGISTRY,
   MEDIATOR: MEDIATOR_REGISTRY,
+  VALIDATOR: VALIDATOR_REGISTRY,
 };
