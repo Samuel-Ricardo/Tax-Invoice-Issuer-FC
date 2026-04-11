e392f74d9fc05fa3923da1b6f30093af459a13e8
samuel_ricardo <samuelricardoofficial@gmail.com>
2026-04-11 05:10:10 -0300

    [ :construction: ] | wip

--- src/@modules/domain/specification/specification.interface.ts
+++ src/@modules/domain/specification/specification.interface.ts
@@ -1,6 +1,6 @@
 export interface Specification<T> {
   isSatisfiedBy(cadidate: T): boolean;
-  and(other: Specification<T>): Specification<T>;
-  or(other: Specification<T>): Specification<T>;
-  not(): Specification<T>;
+  and(other: Specification<T>, cadidate: T): boolean;
+  or(other: Specification<T>, cadidate: T): boolean;
+  not(cadidate: T): boolean;
 }
