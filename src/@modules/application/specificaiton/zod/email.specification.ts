35f3dc73588ed38fa15106140c4e759fffe20c73
samuel_ricardo <samuelricardoofficial@gmail.com>
2026-04-20 04:12:35 -0300

    [ :safety_vest: ] | create: specification - mail (@module::application)

---
+++ src/@modules/application/specificaiton/zod/email.specification.ts
@@ -0,0 +1,33 @@
+import { inject, injectable } from "inversify";
+import { MODULE } from "../../../app.registry";
+import { Invoice } from "../../../domain/entity/invoice.entity";
+import { Specification } from "../../../domain/specification/specification.interface";
+import { Validator } from "../../../domain/validator/validator.interface";
+import { ZodValidator } from "../../../infra/validator/zod/zod.validator";
+import { date } from "zod";
+
+@injectable()
+export class EmailSpecificationZod implements Specification<Invoice> {
+  constructor(
+    @inject(MODULE.INFRA.VALIDATOR.ZOD)
+    private readonly validator: ZodValidator<Invoice>,
+  ) {
+    this.setupRules();
+  }
+
+  isSatisfiedBy(cadidate: Invoice): boolean {
+    throw new Error("Method not implemented.");
+  }
+
+  and(other: Specification<Invoice>, cadidate: Invoice): boolean {
+    throw new Error("Method not implemented.");
+  }
+
+  or(other: Specification<Invoice>, cadidate: Invoice): boolean {
+    throw new Error("Method not implemented.");
+  }
+
+  not(cadidate: Invoice): boolean {
+    throw new Error("Method not implemented.");
+  }
+}
