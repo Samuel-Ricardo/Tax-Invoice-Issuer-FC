94eb3136278c849b13b284de1e0a3be83b7fa042
samuel_ricardo <samuelricardoofficial@gmail.com>
2026-04-20 02:45:42 -0300

    [ :safety_vest: ] | implements: specification - invoice > validation (@module::application)

--- src/@modules/application/specificaiton/zod/invoice.specification.ts
+++ src/@modules/application/specificaiton/zod/invoice.specification.ts
@@ -1,23 +1,29 @@
+import { inject } from "inversify";
 import { Invoice } from "../../../domain/entity/invoice.entity";
 import { Specification } from "../../../domain/specification/specification.interface";
 import { ZodValidator } from "../../../infra/validator/zod/zod.validator";
+import { MODULE } from "../../../app.registry";
+import { InvoiceDTO } from "../../../domain/DTO/invoice.dto";
 
-export class InvoiceSpecificationZod implements Specification<Invoice> {
-  constructor(private readonly validator: ZodValidator<Invoice>) {
+export class InvoiceSpecificationZod implements Specification<InvoiceDTO> {
+  constructor(
+    @inject(MODULE.INFRA.VALIDATOR.ZOD)
+    private readonly validator: ZodValidator<InvoiceDTO>,
+  ) {
     this.setupRules();
   }
 
-  isSatisfiedBy(cadidate: Invoice): boolean {
+  isSatisfiedBy(cadidate: InvoiceDTO): boolean {
     return this.validator.validate(cadidate).isValid;
   }
 
-  and(other: Specification<Invoice>, cadidate: Invoice): boolean {
+  and(other: Specification<InvoiceDTO>, cadidate: InvoiceDTO): boolean {
     return this.isSatisfiedBy(cadidate) && other.isSatisfiedBy(cadidate);
   }
-  or(other: Specification<Invoice>, cadidate: Invoice): boolean {
+  or(other: Specification<InvoiceDTO>, cadidate: InvoiceDTO): boolean {
     return this.isSatisfiedBy(cadidate) || other.isSatisfiedBy(cadidate);
   }
-  not(cadidate: Invoice): boolean {
+  not(cadidate: InvoiceDTO): boolean {
     return !this.isSatisfiedBy(cadidate);
   }
 
