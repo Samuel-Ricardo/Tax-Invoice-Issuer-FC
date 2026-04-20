319c469d6cffa9f23782fa4e7c6813c9d8b770c5
samuel_ricardo <samuelricardoofficial@gmail.com>
2026-04-20 02:52:17 -0300

    [ :syringe: ] | update: module - application > specification (@module)

--- src/@modules/application/application.module.ts
+++ src/@modules/application/application.module.ts
@@ -1,6 +1,7 @@
 import { CONTROLLER_MODULE } from "./controller/controller.module";
 import { REPOSITORY_MODULE } from "./repository/repository.module";
 import { SERVICE_MODULE } from "./service/service.module";
+import { SPECIFICATION_MODULE } from "./specificaiton/specification.module";
 import { USE_CASE_MODULE } from "./use-case/use-case.module";
 
 export const APPLICATION_MODULE = [
@@ -8,4 +9,5 @@ export const APPLICATION_MODULE = [
   ...USE_CASE_MODULE,
   ...SERVICE_MODULE,
   ...CONTROLLER_MODULE,
+  ...SPECIFICATION_MODULE,
 ];
