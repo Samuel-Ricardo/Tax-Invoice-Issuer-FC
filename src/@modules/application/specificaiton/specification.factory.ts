cff5dfd7d83b89ba00ded843261bc1fed5da844d
samuel_ricardo <samuelricardoofficial@gmail.com>
2026-04-20 02:50:19 -0300

    [ :syringe: ] | create: factory - specification (@module::application)

---
+++ src/@modules/application/specificaiton/specification.factory.ts
@@ -0,0 +1,11 @@
+import { loads } from "../../../@utils/module/load.util";
+import { SPECIFICATION_MODULE } from "./specification.module";
+import { SPECIFICATION_REGISTRY } from "./specification.registry";
+import { InvoiceSpecificationZod } from "./zod/invoice.specification";
+
+const _MODULE = loads(SPECIFICATION_MODULE);
+
+export const SPECIFICATION_FACTORY = {
+  INVOICE: () =>
+    _MODULE.get<InvoiceSpecificationZod>(SPECIFICATION_REGISTRY.INVOICE),
+};
