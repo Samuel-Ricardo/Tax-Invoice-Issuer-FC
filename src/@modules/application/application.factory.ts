d685c54d7113f450625daacc15c6bd632d5ead0f
samuel_ricardo <samuelricardoofficial@gmail.com>
2026-04-20 02:52:42 -0300

    [ :syringe: ] | update: factory - application > specification (@module)

--- src/@modules/application/application.factory.ts
+++ src/@modules/application/application.factory.ts
@@ -1,6 +1,7 @@
 import { CONTROLLER_FACTORY } from "./controller/controller.factory";
 import { REPOSITORY_FACTORY } from "./repository/repository.factory";
 import { SERVICE_FACTORY } from "./service/service.factory";
+import { SPECIFICATION_FACTORY } from "./specificaiton/specification.factory";
 import { USE_CASE_FACTORY } from "./use-case/use-case.factory";
 
 export const APPLICATION_FACTORY = {
@@ -8,4 +9,5 @@ export const APPLICATION_FACTORY = {
   REPOSITORY: REPOSITORY_FACTORY,
   SERVICE: SERVICE_FACTORY,
   USE_CASE: USE_CASE_FACTORY,
+  SPECIFICATION: SPECIFICATION_FACTORY,
 };
