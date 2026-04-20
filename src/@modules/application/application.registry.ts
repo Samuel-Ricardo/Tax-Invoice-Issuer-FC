94a13168c1d016e62832199449b3038238458dfc
samuel_ricardo <samuelricardoofficial@gmail.com>
2026-04-20 02:51:55 -0300

    [ :syringe: ] | update: registry - application > specification (@module)

--- src/@modules/application/application.registry.ts
+++ src/@modules/application/application.registry.ts
@@ -1,6 +1,7 @@
 import { CONTROLLER_REGISTRY } from "./controller/controller.registry";
 import { REPOSITORY_REGISTRY } from "./repository/repository.registry";
 import { SERVICE_REGISTRY } from "./service/service.registry";
+import { SPECIFICATION_REGISTRY } from "./specificaiton/specification.registry";
 import { USE_CASE_REGISTRY } from "./use-case/use-case.registry";
 
 export const APPLICATION_REGISTRY = {
@@ -8,4 +9,5 @@ export const APPLICATION_REGISTRY = {
   REPOSITORY: REPOSITORY_REGISTRY,
   SERVICE: SERVICE_REGISTRY,
   USE_CASE: USE_CASE_REGISTRY,
+  SPECIFICATION: SPECIFICATION_REGISTRY,
 };
