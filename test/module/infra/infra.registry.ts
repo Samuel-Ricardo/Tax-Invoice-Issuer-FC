153d3e8dc10b4c91ad4aabdb4cfb506510c95ecb
samuel_ricardo <samuelricardoofficial@gmail.com>
2026-05-01 04:17:49 -0300

    [ :syringe: ] | create: registry - infra (@module::test)

---
+++ test/module/infra/infra.registry.ts
@@ -0,0 +1,7 @@
+import { TEST_CONFIG_REGISTRY } from "./config/config.registry";
+import TEST_ENGINE_REGISTRY from "./engine/engine.registry";
+
+export const TEST_INFRA_REGISTRY = {
+  CONFIG: TEST_CONFIG_REGISTRY,
+  ENGINE: TEST_ENGINE_REGISTRY,
+};
