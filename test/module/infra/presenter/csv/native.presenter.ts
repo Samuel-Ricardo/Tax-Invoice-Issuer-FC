1f22d042e12f59c6b51698a1f1e41945a92fbeda
samuel_ricardo <samuelricardoofficial@gmail.com>
2026-05-18 12:06:05 -0300

    [ :clown_face: ] | mock: presenter - csv (@mock::module::infra)

---
+++ test/module/infra/presenter/csv/native.presenter.ts
@@ -0,0 +1,4 @@
+import { mockDeep } from "jest-mock-extended";
+import CsvPresenter from "../../../../../src/@modules/infra/presenter/csv/csv.presenter";
+
+export const MockCsvPresenter = mockDeep<CsvPresenter>();
