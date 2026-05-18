b61500d089924d8912be583f0512a5dd1c67afa8
samuel_ricardo <samuelricardoofficial@gmail.com>
2026-05-18 04:56:23 -0300

    [ :clown_face: ] | mock: presenter - json (@mock::module::infra)

---
+++ test/module/infra/presenter/json/native.presenter.ts
@@ -0,0 +1,4 @@
+import { mockDeep } from "jest-mock-extended";
+import { JsonPresenter } from "../../../../../src/@modules/infra/presenter/json/json.presenter";
+
+export const MockJsonPresenter = mockDeep<JsonPresenter>();
