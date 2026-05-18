5c97a3f8568088613a2579c7b20b5a325a1d3bbc
samuel_ricardo <samuelricardoofficial@gmail.com>
2026-05-18 12:02:51 -0300

    [ :syringe: ] | update: module - presenter > json [native] (@mock::module::infra)

--- test/module/infra/presenter/presenter.module.ts
+++ test/module/infra/presenter/presenter.module.ts
@@ -1,7 +1,9 @@
 import { ContainerModule } from "inversify";
 import { MOCK_PRESENTER_REGISTRY } from "./presenter.registry";
 import { MockCsvPresenter } from "./csv/native.presenter";
+import { MockJsonPresenter } from "./json/native.presenter";
 
 export const MOCK_PRESENTER_MODULE = new ContainerModule(({ bind }) => {
   bind(MOCK_PRESENTER_REGISTRY.CSV.NATIVE).toConstantValue(MockCsvPresenter);
+  bind(MOCK_PRESENTER_REGISTRY.JSON.NATIVE).toConstantValue(MockJsonPresenter);
 });
