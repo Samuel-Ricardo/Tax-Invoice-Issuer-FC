4bad76c7e8676f51b6227a22e94df43f241a7a7d
samuel_ricardo <samuelricardoofficial@gmail.com>
2026-05-20 01:11:00 -0300

    [ :syringe: ] | create: factory - server > http [express] (@mock::module::infra)

---
+++ test/module/infra/server/http/http.factory.ts
@@ -0,0 +1,18 @@
+import { loads } from "../../../../../src/@utils/module/load.util";
+import { TEST_HTTP_SERVER_MODULE } from "./http.module";
+import { TEST_HTTP_SERVER_REGISTRY } from "./http.registry";
+
+const _MODULE = loads(TEST_HTTP_SERVER_MODULE);
+
+export const TEST_HTTP_SERVER_FACTORY = {
+  SERVER: {
+    HTTP: {
+      EXPRESS: {
+        MOCK: () =>
+          _MODULE.get(TEST_HTTP_SERVER_REGISTRY.SERVER.HTTP.EXPRESS.MOCK),
+        SIMULATE: () =>
+          _MODULE.get(TEST_HTTP_SERVER_REGISTRY.SERVER.HTTP.EXPRESS.SIMULATE),
+      },
+    },
+  },
+};
