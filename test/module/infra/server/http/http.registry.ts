1a5ecd3855b4e9f1aa0b5e44f3f42dffe1b3dc4d
samuel_ricardo <samuelricardoofficial@gmail.com>
2026-05-20 00:11:07 -0300

    [ :syringe: ] | create: registry - server > http (@mock::module::infra)

---
+++ test/module/infra/server/http/http.registry.ts
@@ -0,0 +1,10 @@
+export const TEST_HTTP_REGISTRY = {
+  SERVER: {
+    HTTP: {
+      EXPRESS: {
+        MOCK: Symbol.for("MOCK::MODULE:INFRA:SERVER:HTTP:EXPRESS"),
+        SIMULATE: Symbol.for("SIMULATE::MODULE:INFRA:SERVER:HTTP:EXPRESS"),
+      },
+    },
+  },
+};
