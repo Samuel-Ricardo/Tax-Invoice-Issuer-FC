38803e446600350c461aae16a6b55fd2da8ff311
samuel_ricardo <samuelricardoofficial@gmail.com>
2026-05-20 00:10:27 -0300

    [ :adhesive_bandage: ] | fix: import - module > infra [engine] (@mock::module::infra)

--- test/module/infra/infra.registry.ts
+++ test/module/infra/infra.registry.ts
@@ -1,5 +1,5 @@
 import { TEST_CONFIG_REGISTRY } from "./config/config.registry";
-import TEST_ENGINE_REGISTRY from "./engine/engine.registry";
+import { TEST_ENGINE_REGISTRY } from "./engine/engine.registry";
 
 export const TEST_INFRA_REGISTRY = {
   CONFIG: TEST_CONFIG_REGISTRY,
