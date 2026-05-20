049b58158856f19ada3845c650fb30e88ab2a944
samuel_ricardo <samuelricardoofficial@gmail.com>
2026-05-20 01:13:05 -0300

    [ :syringe: ] | update: registry - infra > server (@module::test)

--- test/module/infra/infra.registry.ts
+++ test/module/infra/infra.registry.ts
@@ -1,7 +1,9 @@
 import { TEST_CONFIG_REGISTRY } from "./config/config.registry";
 import { TEST_ENGINE_REGISTRY } from "./engine/engine.registry";
+import { TEST_HTTP_SERVER_REGISTRY } from "./server/http/http.registry";
 
 export const TEST_INFRA_REGISTRY = {
   CONFIG: TEST_CONFIG_REGISTRY,
   ENGINE: TEST_ENGINE_REGISTRY,
+  SERVER: TEST_HTTP_SERVER_REGISTRY,
 };
