2823ec2f156f1d01f2f34b9ed6cb507fa67476a8
samuel_ricardo <samuelricardoofficial@gmail.com>
2026-05-21 04:01:49 -0300

    [ :syringe: ] | update: factory - infra > server (@module::test)

--- test/module/infra/infra.factory.ts
+++ test/module/infra/infra.factory.ts
@@ -1,7 +1,9 @@
 import { TEST_CONFIG_FACTORY } from "./config/config.factory";
 import { TEST_ENGINE_FACTORY } from "./engine/engine.factory";
+import { TEST_HTTP_SERVER_FACTORY } from "./server/http/http.factory";
 
 export const TEST_INFRA_FACTORY = {
   CONFIG: TEST_CONFIG_FACTORY,
   ENGINE: TEST_ENGINE_FACTORY,
+  SERVER: TEST_HTTP_SERVER_FACTORY,
 };
