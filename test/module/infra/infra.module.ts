41561e7b979534aca878b9be1df6f69dcd3fce59
samuel_ricardo <samuelricardoofficial@gmail.com>
2026-05-22 03:36:43 -0300

    [ :syringe: ] | update: module - infra > validator (@test::module)

--- test/module/infra/infra.module.ts
+++ test/module/infra/infra.module.ts
@@ -1,9 +1,11 @@
 import { TEST_CONFIG_MODULE } from "./config/config.module";
 import { TEST_ENGINE_MODULE } from "./engine/engine.module";
 import { TEST_HTTP_SERVER_MODULE } from "./server/http/http.module";
+import { TEST_VALIDATOR_MODULE } from "./validator/validator.module";
 
 export const TEST_INFRA_MODULE = [
   TEST_CONFIG_MODULE,
   ...TEST_ENGINE_MODULE,
   ...TEST_HTTP_SERVER_MODULE,
+  ...TEST_VALIDATOR_MODULE,
 ];
