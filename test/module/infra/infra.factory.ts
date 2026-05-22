388cfe0c9e60861ae664554d0154b2be1b301673
samuel_ricardo <samuelricardoofficial@gmail.com>
2026-05-22 03:37:06 -0300

    [ :syringe: ] | update: factory - infra > validator (@test::module)

--- test/module/infra/infra.factory.ts
+++ test/module/infra/infra.factory.ts
@@ -1,9 +1,11 @@
 import { TEST_CONFIG_FACTORY } from "./config/config.factory";
 import { TEST_ENGINE_FACTORY } from "./engine/engine.factory";
 import { TEST_HTTP_SERVER_FACTORY } from "./server/http/http.factory";
+import { TEST_VALIDATOR_FACTORY } from "./validator/validator.factory";
 
 export const TEST_INFRA_FACTORY = {
   CONFIG: TEST_CONFIG_FACTORY,
   ENGINE: TEST_ENGINE_FACTORY,
   SERVER: TEST_HTTP_SERVER_FACTORY,
+  VALIDATION: TEST_VALIDATOR_FACTORY,
 };
