48cb824727ed92ec4c5c421607d13507d0aa1a78
samuel_ricardo <samuelricardoofficial@gmail.com>
2026-05-22 03:35:49 -0300

    [ :syringe: ] | update: registry - infra > validator (@test::module)

--- test/module/infra/infra.registry.ts
+++ test/module/infra/infra.registry.ts
@@ -1,9 +1,11 @@
 import { TEST_CONFIG_REGISTRY } from "./config/config.registry";
 import { TEST_ENGINE_REGISTRY } from "./engine/engine.registry";
 import { TEST_HTTP_SERVER_REGISTRY } from "./server/http/http.registry";
+import { TEST_VALIDATOR_REGISTRY } from "./validator/validator.registry";
 
 export const TEST_INFRA_REGISTRY = {
   CONFIG: TEST_CONFIG_REGISTRY,
   ENGINE: TEST_ENGINE_REGISTRY,
   SERVER: TEST_HTTP_SERVER_REGISTRY,
+  VALIDATION: TEST_VALIDATOR_REGISTRY,
 };
