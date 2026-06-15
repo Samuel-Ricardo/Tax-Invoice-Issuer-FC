20ee917d496edc17464736cda44bf6508b9300d1
samuel_ricardo <samuelricardoofficial@gmail.com>
2026-06-15 04:40:01 -0300

    [ :white_check_mark: ] | test: E2E - server > health check [close:database] (@module::test)

--- test/E2E/server.spec.ts
+++ test/E2E/server.spec.ts
@@ -2,8 +2,11 @@ import "reflect-metadata";
 
 import supertest from "supertest";
 import { MODULES } from "../../src/app";
+import { shutdownDatabase } from "../util/database.util";
 
 describe("[E2E] | SERVER", () => {
+  afterAll(async () => await shutdownDatabase());
+
   it("[E2E] | HEALTH CHECK", async () => {
     const response = await supertest(
       MODULES.INFRA.ENGINE.SERVER.HTTP.EXPRESS._(),
