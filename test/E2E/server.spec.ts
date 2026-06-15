47bef509ebf7441f114357ca022ba8261db98649
samuel_ricardo <samuelricardoofficial@gmail.com>
2026-06-15 01:47:18 -0300

    [ :white_check_mark: ] | test: E2E - server > health check (@module::test)

--- test/E2E/server.spec.ts
+++ test/E2E/server.spec.ts
@@ -4,11 +4,12 @@ import supertest from "supertest";
 import { MODULES } from "../../src/app";
 
 describe("[E2E] | SERVER", () => {
-  it("[E2E] | GENERATE [INVOICE]", async () => {
+  it("[E2E] | HEALTH CHECK", async () => {
     const response = await supertest(
       MODULES.INFRA.ENGINE.SERVER.HTTP.EXPRESS._(),
-    ).get("/invoice");
+    ).get("/");
 
     expect(response.status).toBe(200);
+    expect(response.body).toEqual({ hello: "world" });
   });
 });
