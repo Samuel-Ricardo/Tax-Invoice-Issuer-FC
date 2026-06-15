f76e837f4b6589c63863dcd2356b183820cb985e
samuel_ricardo <samuelricardoofficial@gmail.com>
2026-06-15 04:30:45 -0300

    [ :syringe: ] | update: registry - infra > presenter (@test::module)

--- test/module/infra/infra.registry.ts
+++ test/module/infra/infra.registry.ts
@@ -1,6 +1,7 @@
 import { TEST_CONFIG_REGISTRY } from "./config/config.registry";
 import { TEST_ENGINE_REGISTRY } from "./engine/engine.registry";
 import { TEST_MEDIATOR_REGISTRY } from "./mediator/mediator.registry";
+import { MOCK_PRESENTER_REGISTRY } from "./presenter/presenter.registry";
 import { TEST_HTTP_SERVER_REGISTRY } from "./server/http/http.registry";
 import { TEST_VALIDATOR_REGISTRY } from "./validator/validator.registry";
 
@@ -10,4 +11,5 @@ export const TEST_INFRA_REGISTRY = {
   MEDIATOR: TEST_MEDIATOR_REGISTRY,
   SERVER: TEST_HTTP_SERVER_REGISTRY,
   VALIDATION: TEST_VALIDATOR_REGISTRY,
+  PRESENTER: MOCK_PRESENTER_REGISTRY,
 };
