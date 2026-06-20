d11c32c6f4a8d91bbf164c2ed2bed47fccc24926
samuel_ricardo <samuelricardoofficial@gmail.com>
2026-06-20 02:38:08 -0300

    [ :clown_face: ] | mock: application - service > email (@mock::module)

--- test/module/application/service/email/email.service.ts
+++ test/module/application/service/email/email.service.ts
@@ -3,10 +3,13 @@ import { EmailServiceImpl } from "../../../../../src/@modules/application/servic
 import { ResolutionContext } from "inversify";
 import { TEST_MODULE } from "../../../app.registry";
 import { SendInvoiceEmailUseCase } from "../../../../../src/@modules/domain/use-case/email/send/invoice.use-case";
+import { SimulatedEmailService } from "../../../../@types/service/email/simulated.type";
 
 export const mockEmailService = mockDeep<EmailServiceImpl>();
 
-export const simulateEmailService = (module: ResolutionContext) => {
+export const simulateEmailService = (
+  module: ResolutionContext,
+): SimulatedEmailService => {
   const use_case = module.get<DeepMockProxy<SendInvoiceEmailUseCase>>(
     TEST_MODULE.APPLICATION.USE_CASE.EMAIL.SEND.INVOICE.SIMULATE,
   );
