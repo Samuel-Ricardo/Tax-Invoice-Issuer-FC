81c7c2de177f16a89b184dd2f0947c4a9ba93491
samuel_ricardo <samuelricardoofficial@gmail.com>
2026-06-20 02:19:28 -0300

    [ :adhesive_bandage: ] | fix: module - controller > test [context:import] (@module::application)

--- src/@modules/application/controller/controller.factory.ts
+++ src/@modules/application/controller/controller.factory.ts
@@ -6,6 +6,8 @@ import { CONTROLLER_REGISTRY } from "./controller.registry";
 
 const _MODULE = loads(CONTROLLER_MODULE);
 
+export const CONTROLLER_CONTAINER = _MODULE;
+
 export const CONTROLLER_FACTORY = {
   INVOICE: () => _MODULE.get<Controller>(CONTROLLER_REGISTRY.INVOICE),
   EMAIL: () => _MODULE.get<Controller>(CONTROLLER_REGISTRY.EMAIL),
