299b14d1abdfd3d6fd80067876ad29db5f8a3ab0
samuel_ricardo <samuelricardoofficial@gmail.com>
2026-06-21 01:54:25 -0300

    [ :syringe: ] | update: factory - infra > presenter (@test::module)

--- test/module/infra/infra.factory.ts
+++ test/module/infra/infra.factory.ts
@@ -1,5 +1,6 @@
 import { TEST_CONFIG_FACTORY } from "./config/config.factory";
 import { TEST_ENGINE_FACTORY } from "./engine/engine.factory";
+import { MOCK_PRESENTER_FACTORY } from "./presenter/presenter.factory";
 import { TEST_HTTP_SERVER_FACTORY } from "./server/http/http.factory";
 import { TEST_VALIDATOR_FACTORY } from "./validator/validator.factory";
 
@@ -8,4 +9,5 @@ export const TEST_INFRA_FACTORY = {
   ENGINE: TEST_ENGINE_FACTORY,
   SERVER: TEST_HTTP_SERVER_FACTORY,
   VALIDATION: TEST_VALIDATOR_FACTORY,
+  PRESEENTER: MOCK_PRESENTER_FACTORY,
 };
