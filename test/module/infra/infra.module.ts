67c6ce209f76cf430f4df24e0139ea0341b68966
samuel_ricardo <samuelricardoofficial@gmail.com>
2026-06-21 01:53:24 -0300

    [ :syringe: ] | update: module - infra > presenter (@test::module)

--- test/module/infra/infra.module.ts
+++ test/module/infra/infra.module.ts
@@ -1,13 +1,15 @@
 import { TEST_CONFIG_MODULE } from "./config/config.module";
 import { TEST_ENGINE_MODULE } from "./engine/engine.module";
 import { TEST_MEDIATOR_MODULE } from "./mediator/mediator.module";
+import { MOCK_PRESENTER_MODULE } from "./presenter/presenter.module";
 import { TEST_HTTP_SERVER_MODULE } from "./server/http/http.module";
 import { TEST_VALIDATOR_MODULE } from "./validator/validator.module";
 
 export const TEST_INFRA_MODULE = [
   TEST_CONFIG_MODULE,
+  TEST_MEDIATOR_MODULE,
+  MOCK_PRESENTER_MODULE,
   ...TEST_ENGINE_MODULE,
-  ...TEST_MEDIATOR_MODULE,
   ...TEST_HTTP_SERVER_MODULE,
   ...TEST_VALIDATOR_MODULE,
 ];
