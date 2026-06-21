a4a8ec7be1a0972c543c7eee3b090b5a5d186afb
samuel_ricardo <samuelricardoofficial@gmail.com>
2026-06-21 01:45:37 -0300

    [ :card_file_box: ] | update: util - database > shutdown (@app::test)

--- test/util/database.util.ts
+++ test/util/database.util.ts
@@ -1,25 +1,13 @@
+import { CONTROLLER_CONTAINER } from "../../src/@modules/application/controller/controller.factory";
 import { SQLDatabaseConnection } from "../../src/@modules/infra/engine/database/connection/sql/sql.connection";
-import { loads } from "../../src/@utils/module/load.util";
-import { INFRA_MODULE } from "../../src/@modules/infra/infra.module";
 import { DATABASE_ENGINE_REGISTRY } from "../../src/@modules/infra/engine/database/database.registry";
-
-let _dbConnection: SQLDatabaseConnection | null = null;
-
-function getDbConnection(): SQLDatabaseConnection {
-  if (!_dbConnection) {
-    const container = loads(INFRA_MODULE);
-    _dbConnection = container.get<SQLDatabaseConnection>(
-      DATABASE_ENGINE_REGISTRY.SQL.POSTGRES.PGPROMISE,
-    );
-  }
-  return _dbConnection;
-}
 
 export async function shutdownDatabase() {
   try {
-    const db = getDbConnection();
+    const db = CONTROLLER_CONTAINER.get<SQLDatabaseConnection>(
+      DATABASE_ENGINE_REGISTRY.SQL.POSTGRES.PGPROMISE,
+    );
     await db.close();
-    _dbConnection = null;
   } catch (error) {
     console.error("Erro ao fechar conexão do banco (teardown):", error);
   }
