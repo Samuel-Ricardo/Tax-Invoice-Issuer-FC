1db22a31d08509429f6aeee3d6804876864a28cd
samuel_ricardo <samuelricardoofficial@gmail.com>
2026-06-22 04:05:53 -0300

    [ :white_check_mark: ] | test: controller - invoice > should be [defined] (@test::unit)

--- test/unit/controller/invoice.spec.ts
+++ test/unit/controller/invoice.spec.ts
@@ -1,5 +1,13 @@
+import { TEST_MODULES } from "../../module/app.factory";
+
 describe("[INVOICE] - CONTROLLER", () => {
-  it("should be defined", () => {
-    expect(true).toBe(true);
+  it("[UNIT] | Controller created with correct dependencies", () => {
+    const module = TEST_MODULES.APPLICATION.CONTROLLER.INVOICE.SIMULATE();
+
+    expect(module.controller).toBeDefined();
+    expect(module.server).toBeDefined();
+    expect(module.presenter).toBeDefined();
+    expect(module.service).toBeDefined();
+    expect(module.specification).toBeDefined();
   });
 });
