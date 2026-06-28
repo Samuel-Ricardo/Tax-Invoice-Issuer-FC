5e6f8d5954d8f0441f6f2dd5601069375ff1ba7d
samuel_ricardo <samuelricardoofficial@gmail.com>
2026-06-28 03:08:44 -0300

    [ :syringe: ] | update: registry - application > controller (@test::module)

--- test/module/application/application.registry.ts
+++ test/module/application/application.registry.ts
@@ -1,3 +1,4 @@
+import { TEST_CONTROLLER_REGISTRY } from "./controller/controller.registry";
 import { TEST_REPOSITORY_REGISTRY } from "./repository/repository.registry";
 import { TEST_SERVICE_REGISTRY } from "./service/service.registry";
 import { TEST_SPECIFICATION_REGISTRY } from "./specification/specification.registry";
@@ -8,4 +9,5 @@ export const TEST_APPLICATION_REGISTRY = {
   USE_CASE: TEST_USE_CASE_REGISTRY,
   SERVICE: TEST_SERVICE_REGISTRY,
   SPECIFICATION: TEST_SPECIFICATION_REGISTRY,
+  CONTROLLER: TEST_CONTROLLER_REGISTRY,
 };
