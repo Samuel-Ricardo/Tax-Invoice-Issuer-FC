397bbc13bb051ba21a08b838a1bd69987bee18a2
samuel_ricardo <samuelricardoofficial@gmail.com>
2026-07-04 15:32:45 -0300

    [ :syringe: ] | update: factory - repository > types [simulated] (@module::application)

--- test/module/application/repository/repository.factory.ts
+++ test/module/application/repository/repository.factory.ts
@@ -1,6 +1,8 @@
 import { loads } from "../../../../src/@utils/module/load.util";
 import { TEST_REPOSITORY_MODULE } from "./repository.module";
 import { TEST_REPOSITORY_REGISTRY } from "./repository.registry";
+import { SimulatedContractRepository } from "../../../@types/repository/contract/simulated.type";
+import { SimulatedPaymentRepository } from "../../../@types/repository/payment/simulated.type";
 
 const _MODULE = loads(TEST_REPOSITORY_MODULE);
 
@@ -9,12 +11,16 @@ export const TEST_REPOSITORY_FACTORY = {
     CONTRACT: {
       MOCK: () => _MODULE.get(TEST_REPOSITORY_REGISTRY.SQL.CONTRACT.MOCK),
       SIMULATE: () =>
-        _MODULE.get(TEST_REPOSITORY_REGISTRY.SQL.CONTRACT.SIMULATE),
+        _MODULE.get(
+          TEST_REPOSITORY_REGISTRY.SQL.CONTRACT.SIMULATE,
+        ) as SimulatedContractRepository,
     },
     PAYMENT: {
       MOCK: () => _MODULE.get(TEST_REPOSITORY_REGISTRY.SQL.PAYMENT.MOCK),
       SIMULATE: () =>
-        _MODULE.get(TEST_REPOSITORY_REGISTRY.SQL.PAYMENT.SIMULATE),
+        _MODULE.get(
+          TEST_REPOSITORY_REGISTRY.SQL.PAYMENT.SIMULATE,
+        ) as SimulatedPaymentRepository,
     },
   },
 };
